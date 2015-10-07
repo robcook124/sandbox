7da2e5b750494bd7b8b24a6d42e2ba96021f446e
robcook124 <robcook124@gmail.com>
2015-10-07 19:29:58 -0400

    Jenkins Backup

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo -u jenkins rm -r backup
-sudo -u jenkins mv FULL* backup
-sudo -u jenkins git add .
-sudo -u jenkins git commit -m "Jenkins Backup"
-sudo -u jenkins git push origin master
+rm -r backup
+mv FULL* backup
+git add .
+git commit -m "Jenkins Backup"
+git push origin master
