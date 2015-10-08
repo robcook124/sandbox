caa354be5ca980a40a83801c59fc292344821177
robcook124 <robcook124@gmail.com>
2015-10-07 20:11:02 -0400

    Jenkins Backup

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /var/lib/jenkins/backups/sandbox
-chmod 777 FULL* backup
+#chmod 777 FULL* backup
 sudo -u jenkins rm -r backup
 sudo -u jenkins mv FULL* backup
 sudo -u jenkins git add .
